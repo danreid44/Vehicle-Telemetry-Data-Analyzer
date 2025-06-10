100007d6969752e120d28ac7c03634cb2f6a52d5
Dan Reid <danreid802@gmail.com>
2025-06-09 20:40:11 -0400

    Updated Bash file

--- clear.sh
+++ clear.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-# Delete telemetry CSV
-rm -f data/telemetry.csv
+# Bash script to clear telemetry CSV and database 
+# Execute file: ./clear.sh
 
-# Delete telemetry database
-rm -f db/telemetry.db
+# Delete telemetry CSV in data directory
+if [ -e data/telemetry.csv ]; then
+    rm data/telemetry.csv
+    echo "Cleared telemetry CSV."
+else
+    echo "Error: data/telemetry.csv does not exist."
+fi
 
-echo "Cleared telemetry CSV and database."
+# Delete telemetry database in SQLite
+if [ -e db/telemetry.db ]; then
+    rm db/telemetry.db
+    echo "Cleared telemetry database."
+else
+    echo "Error: db/telemetry.db does not exist."
+fi
+
